ebe5132e34466386c070dcd823111d8e9e5acb78
Rushikesh Auti <autirushikesh758@gmail.com>
2026-05-15 14:08:23 +0530

    Add UI components for StyleKart application

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const { getStoredItems, storeItems } = require('./data/items');
+
 const app = express();
 
 app.use(bodyParser.json());
