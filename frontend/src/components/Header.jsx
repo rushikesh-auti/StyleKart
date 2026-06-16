1c6862af55ee83b0df30a7e6ae9cc37399dc19a6
Rushikesh Auti <autirushikesh758@gmail.com>
2026-06-16 14:26:19 +0530

    Responsive mobile-first header redesign with navigation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,11 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { BsFillPersonFill } from "react-icons/bs";
 import { FaFaceGrinHearts, FaBagShopping } from "react-icons/fa6";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { searchActions } from "../store/searchSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
-
   const [menuOpen, setMenuOpen] = useState(false);
 
   const bag = useSelector((store) => store.bag || []);
@@ -23,90 +22,117 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <div className="logo_container">
-        <Link to="/" onClick={closeMenu}>
-          <img
-            className="stylekart_home"
-            src="/images/stylekart.png"
-            alt="StyleKart"
-          />
-        </Link>
-      </div>
-
-      <div
-        className="menu_icon"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        {menuOpen ? <FaTimes /> : <FaBars />}
-      </div>
-
-      <nav className={`nav_bar ${menuOpen ? "active" : ""}`}>
-        <Link to="/" >
-          Men
-        </Link>
-
-        <Link to="/" >
-          Women
-        </Link>
-
-        <Link to="/" >
-          Kids
-        </Link>
-
-        <Link to="/" >
-          Home & Living
-        </Link>
-
-        <Link to="/" >
-          Beauty
-        </Link>
-      </nav>
-
-      <div className="search_bar">
-        <input
-          type="text"
-          className="search_input"
-          placeholder="Search products..."
-          onChange={handleSearch}
-        />
-      </div>
-
-      <div className="action_bar">
-        <div className="action_container">
-          <BsFillPersonFill size={20} />
-          <span className="action_name">Profile</span>
+    <>
+      <header>
+        <div className="logo_container">
+          <Link to="/" onClick={closeMenu}>
+            <img
+              className="stylekart_home"
+              src="/images/stylekart.png"
+              alt="StyleKart"
+            />
+          </Link>
         </div>
 
-        <Link
-          className="action_container"
-          to="/wishlist"
-          onClick={closeMenu}
+        {/* <div
+          className="menu_icon"
+          onClick={() => setMenuOpen(!menuOpen)}
         >
-          <FaFaceGrinHearts size={20} />
+          {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </div> */}
 
-          <span className="action_name">Wishlist</span>
+        <nav className={`nav_bar ${menuOpen ? "active" : ""}`}>
+          <Link to="/" >
+            Men
+          </Link>
 
-          <span className="bag-item-count">
-            {wishlist.length}
-          </span>
+          <Link to="/" >
+            Women
+          </Link>
+
+          <Link to="/" >
+            Kids
+          </Link>
+
+          <Link to="/" >
+            Beauty
+          </Link>
+        </nav>
+
+        <div className="search_bar">
+          <input
+            type="text"
+            className="search_input"
+            placeholder="Search products..."
+            onChange={handleSearch}
+          />
+        </div>
+
+        <div className="action_bar">
+          <div className="action_container">
+            <BsFillPersonFill size={20} />
+            <span className="action_name">Profile</span>
+          </div>
+
+          <Link
+            className="action_container"
+            to="/wishlist"
+            onClick={closeMenu}
+          >
+            <FaFaceGrinHearts size={20} />
+
+            <span className="action_name">Wishlist</span>
+
+            <span className="bag-item-count">
+              {wishlist.length}
+            </span>
+          </Link>
+
+          <Link
+            className="action_container"
+            to="/bag"
+            onClick={closeMenu}
+          >
+            <FaBagShopping size={20} />
+
+            <span className="action_name">Bag</span>
+
+            <span className="bag-item-count">
+              {bag.length}
+            </span>
+          </Link>
+        </div>
+      </header>
+
+      <nav className="mobile_bottom_nav" role="navigation" aria-label="Mobile">
+        <Link to="/" className="mobile_nav_item" onClick={closeMenu}>
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 11.5L12 4l9 7.5V20a1 1 0 0 1-1 1h-5v-6H9v6H4a1 1 0 0 1-1-1v-8.5z" fill="currentColor" /></svg>
+          <span>Home</span>
         </Link>
 
-        <Link
-          className="action_container"
-          to="/bag"
-          onClick={closeMenu}
-        >
-          <FaBagShopping size={20} />
-
-          <span className="action_name">Bag</span>
-
-          <span className="bag-item-count">
-            {bag.length}
-          </span>
+        <Link to="/" className="mobile_nav_item" onClick={closeMenu}>
+          <FaBars />
+          <span>Categories</span>
         </Link>
-      </div>
-    </header>
+
+        <Link to="/wishlist" className="mobile_nav_item" onClick={closeMenu}>
+          <FaFaceGrinHearts />
+          <span>Wishlist</span>
+          <span className="bag-item-count">{wishlist.length}</span>
+        </Link>
+
+        <Link to="/bag" className="mobile_nav_item" onClick={closeMenu}>
+          <FaBagShopping />
+          <span>Bag</span>
+          <span className="bag-item-count">{bag.length}</span>
+        </Link>
+
+        <Link to="/" className="mobile_nav_item" onClick={closeMenu}>
+          <BsFillPersonFill />
+          <span>Account</span>
+        </Link>
+      </nav>
+    </>
   );
 };
 
