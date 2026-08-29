2c2fb4970dab5b73a5eb2cf7b4f061a34d3dbe41
Rushikesh Auti <autirushikesh758@gmail.com>
2026-08-29 14:45:17 +0530

    Add product deletion controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -91,27 +91,27 @@ exports.updateProduct = async (req, res) => {
 };
 
 // DELETE Product
-// exports.deleteProduct = async (req, res) => {
-//   try {
-//     const product = await Product.findOneAndDelete({
-//       id: req.params.id,
-//     });
+exports.deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findOneAndDelete({
+      id: req.params.id,
+    });
 
-//     if (!product) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "Product not found",
-//       });
-//     }
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
 
-//     res.status(200).json({
-//       success: true,
-//       message: "Product deleted successfully",
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// };
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
