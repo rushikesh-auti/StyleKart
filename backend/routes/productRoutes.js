f22bd69196e4bc0a22e4bdcfe8a66e41a7cc9dca
Rushikesh Auti <autirushikesh758@gmail.com>
2026-08-29 14:45:54 +0530

    Add product deletion route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -18,6 +18,6 @@ router.post("/", createProduct);
 
 router.put("/:id", updateProduct);
 
-// router.delete("/:id", deleteProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
