0db8dbaf663a619e31130c4ef66fb6fb8066d559
Rushikesh Auti <autirushikesh758@gmail.com>
2026-09-07 17:56:28 +0530

    Update product API endpoint

--- frontend/src/components/Fetchitems.jsx
+++ frontend/src/components/Fetchitems.jsx
@@ -16,7 +16,7 @@ const FetchItems = () => {
     dispatch(fetchStatusActions.markFetchingStarted());
 
     // fetch("https://stylekart-7x1q.onrender.com/api/products", { signal })
-    fetch("http://localhost:5000/api/products", { signal })
+    fetch("https://stylekart-inwb.onrender.com/api/products", { signal })
       .then((res) => res.json())
       .then((data) => {
         dispatch(fetchStatusActions.markFetchDone());
