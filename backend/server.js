64ba447f67dad33d1b29714776857252ae820044
Rushikesh Auti <autirushikesh758@gmail.com>
2026-09-11 16:24:51 +0530

    Fix configure backend startup

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 connectDB();
 
