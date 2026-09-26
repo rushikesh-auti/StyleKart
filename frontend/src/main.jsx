4a41b6afedeb0e7bce37b683d7a83406e5e11e7e
Rushikesh Auti <autirushikesh758@gmail.com>
2026-09-26 15:51:52 +0530

    Support React routes on Vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import styleKartStore from "./store/index.js";
@@ -43,7 +43,7 @@ import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import AdminProtectedRoute from "./pages/AdminProtectedRoute.jsx";
 import CustomerOnlyRoute from "./components/CustomerOnlyRoute.jsx";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
